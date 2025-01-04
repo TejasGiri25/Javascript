75b9ec79b5eb2f340dee8efa564ac27416916374
TejasGiri25 <tejasgiri200@gmail.com>
2025-01-04 23:32:04 +0530

    fourth

---
+++ MultiplicationTable.js
@@ -0,0 +1,25 @@
+const z=Number(prompt('Enter the number:'));
+
+var a=z*1;
+var b=z*2; 
+var c=z*3;
+var d=z*4;
+var e=z*5;
+var f=z*6;
+var g=z*7;
+var h=z*8;
+var i=z*9;
+var j=z*10;
+
+document.write("<br> Multiplication table of : " +z+" is... <br> "+" <br>");
+
+document.write(z+" * 1 = "+a+"<br>");
+document.write(z+" * 2 = "+b+"<br>");
+document.write(z+" * 3 = "+c+"<br>");
+document.write(z+" * 4 = "+d+"<br>");
+document.write(z+" * 5 = "+e+"<br>");
+document.write(z+" * 6 = "+f+"<br>");
+document.write(z+" * 7 = "+g+"<br>");
+document.write(z+" * 8 = "+h+"<br>");
+document.write(z+" * 9 = "+i+"<br>");
+document.write(z+" * 10= "+j+"<br>");
